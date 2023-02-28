ed3f4138ff2ba07a13b68524860be2a775aab0d0
bonox2 <illiagoncharov99@gmail.com>
2023-02-28 10:35:42 -0500

    Axois swr calls

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Header from '../components/PageParts/header'
+
 
 export default function Home() {
   return (
@@ -10,7 +10,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
+      <main >
+          <div className="text-red-300">sdfsdf</div>
+      </main>
     </>
   )
 }
