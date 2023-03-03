55190f6dd269083ac380b57dd55c8e686dbd1859
bonox2 <illiagoncharov99@gmail.com>
2023-03-03 15:48:32 -0500

    tailwind css added

--- src/components/PageParts/header.tsx
+++ src/components/PageParts/header.tsx
@@ -14,19 +14,18 @@ export default function Header() {
   return (
     <header>
       <div className="container ml-auto mr-auto flex justify-between items-center flex-row">
-        <div className="logo">
+        <div>
           <Image src="/logo.png" alt="Logo" width={65} height={35} />
         </div>
-        <div className="search_bar_wrapper">
-          <div className="search_bar">
-            <form className="">
-              <input type="text" placeholder="What are you looking for?" />
-              <div className="categories">
-                <button>All categories</button>
-              </div>
-              <button type="submit"></button>
-            </form>
-          </div>
+        <div className="text-sm  ">
+          <form className="flex justify-center items-center flex-row flex-nowrap w-min ">
+                <input type="text" placeholder="What are you looking for?" className='border-0 rounded
+                 border-gray-300 border-style: solid;'/>
+                <div className="whitespace-nowrap flex wrap-nowrap transition-all duration-3000 bg-transparent hover:bg-blue-500 text-blue-400 font-semibold hover:text-white text-intherit  py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                  <button>All categories</button>
+                </div>
+                <button type="submit" className='border-0 rounded border-gray-300 border-style: solid; ' >Search</button>
+          </form>
         </div>
         <div className="headerButtons">
           <Link href="/">Sign in</Link>
