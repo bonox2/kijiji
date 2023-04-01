479987c44356950cd84efe2c4db07f55d9f579d6
bonox2 <illiagoncharov99@gmail.com>
2023-04-01 16:54:07 -0400

    NavList component errors fixed

--- src/components/PageParts/NavList.tsx
+++ src/components/PageParts/NavList.tsx
@@ -1,23 +1,17 @@
 
 
 import useSWR from "swr";
-import { getCategories } from "../../services/api";
+import { getData } from "../../services/api";
 
 export default function NavList() {
     
-  const { data: subCategoriesData } = useSWR("/subcategories", getCategories);
+  const { data: subcategories } = useSWR("/subcategories", getData);
   
-  const subCategoryNames = subCategoriesData?.map(
-    (subCategoryData) => subCategoryData.attributes.name
-  );
-
   return (
     <li
               className="mr-12 py-6 font-medium text-[#373373] border-b-transparent border-b-4
            hover:cursor-pointer hover:border-b-4 hover:border-[#37a864] 
-           box-border relative group"
-              key={categoryId}>
-              {categoryName}
+           box-border relative group">
               {subcategories.length > 0 && (
                 <ul className="absolute top-full left-0 border invisible group-hover:visible">
                   {subcategories.map((subcategory) => {
